7b618ff70c4419830be2f7a2a43fdf8b91a52911
miathedev <mia@metzler.systems>
2022-12-06 16:46:24 +0100

    office_worker_detection_red

--- test/IntelligentOfficeTest.py
+++ test/IntelligentOfficeTest.py
@@ -7,7 +7,26 @@ from IntelligentOfficeError import IntelligentOfficeError
 
 
 class IntelligentOfficeTest(unittest.TestCase):
+    def setUp(self):
+        self.office = IntelligentOffice()
     """
     Define your test cases here
     """
     pass
+
+    @patch("mock.GPIO.input")
+    def test_office_worker_detection_true(self, mock_input):
+        mock_input.return_value = 1023
+        for i in [11, 12, 13, 15]:
+            presence = self.office.check_quadrant_occupancy(i)
+            self.assertTrue(presence)
+
+    @patch("mock.GPIO.input")
+    def test_office_worker_detection_false(self, mock_input):
+        mock_input.return_value = 0
+        for i in [11, 12, 13, 15]:
+            presence = self.office.check_quadrant_occupancy(i)
+            self.assertFalse(presence)
+    def test_office_worker_detection_throws_on_invalid_pin(self):
+        self.assertRaises(IntelligentOfficeError, self.office.check_quadrant_occupancy, 1)
+
