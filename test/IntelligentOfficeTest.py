d948d341452e4a1e0cefc67526bf90161e73aff1
miathedev <mia@metzler.systems>
2022-12-06 17:11:47 +0100

    blinds_manager_red

--- test/IntelligentOfficeTest.py
+++ test/IntelligentOfficeTest.py
@@ -14,6 +14,9 @@ class IntelligentOfficeTest(unittest.TestCase):
     """
     pass
 
+    #######################
+    #Task 1
+    #######################
     @patch("mock.GPIO.input")
     def test_office_worker_detection_true(self, mock_input):
         mock_input.return_value = 1023
@@ -30,3 +33,44 @@ class IntelligentOfficeTest(unittest.TestCase):
     def test_office_worker_detection_throws_on_invalid_pin(self):
         self.assertRaises(IntelligentOfficeError, self.office.check_quadrant_occupancy, 1)
 
+    #######################
+    # Task 2
+    #######################
+
+    @patch.object(RTC, "get_current_time_string")
+    @patch.object(RTC, "get_current_day")
+    def test_blinds_manager_on_time_weekday(self, mock_rtc_day, mock_rtc_time):
+        for (start_time, stop_time) in [("08:00:00", "20:00:00"), ("10:00:00", "22:00:00")]:
+            for weekday in ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"]:
+                mock_rtc_day.return_value = weekday
+
+                #Open
+                mock_rtc_time.return_value = start_time
+
+                self.office.manage_blinds_based_on_time()
+                self.assertTrue(self.office.blinds_open)
+
+                #Close
+                mock_rtc_time.return_value = stop_time
+
+                self.office.manage_blinds_based_on_time()
+                self.assertFalse(self.office.blinds_open)
+
+    @patch.object(RTC, "get_current_time_string")
+    @patch.object(RTC, "get_current_day")
+    def test_blinds_manager_on_time_weekend(self, mock_rtc_day, mock_rtc_time):
+        for (start_time, stop_time) in [("08:00:00", "20:00:00"), ("10:00:00", "22:00:00")]:
+            for weekday in ["SATURDAY", "SUNDAY"]:
+                mock_rtc_day.return_value = weekday
+
+                # Open
+                mock_rtc_time.return_value = start_time
+
+                self.office.manage_blinds_based_on_time()
+                self.assertFalse(self.office.blinds_open)
+
+                # Close
+                mock_rtc_time.return_value = stop_time
+
+                self.office.manage_blinds_based_on_time()
+                self.assertFalse(self.office.blinds_open)
